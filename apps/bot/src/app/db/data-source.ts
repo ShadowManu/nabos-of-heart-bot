7d4df53d475a88cbc008897da3def8a9c304d302
Manuel Pacheco <contact@manuelpacheco.dev>
2022-05-28 21:50:20 -0400

    reorganize entities and migration folders

--- apps/bot/src/app/db/data-source.ts
+++ apps/bot/src/app/db/data-source.ts
@@ -5,5 +5,7 @@ import { env } from '../env';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   logging: true,
+  migrations: ['apps/bot/src/app/db/migrations/*.ts'],
+  entities: ['apps/bot/src/app/db/entities/*.ts'],
   ...env.postgres(),
 });
